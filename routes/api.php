51b6dcdf220efa91aa660dde52a493dc77d5030f
syaikha rachmah <hi.syaikha@gmail.com>
2021-11-24 20:11:09 +0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,25 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/ebook','App\Http\Controllers\BookController@index');
-Route::post('/ebook', 'App\Http\Controllers\BookController@create');
-Route::put('/ebook/{id}', 'App\Http\Controllers\BookController@update');
-Route::delete('/ebook/{id}', 'App\Http\Controllers\BookController@delete');
+Route::get('/me','App\Http\Controllers\AuthController@me');
 
-Route::get('/me', 'App\Http\Controllers\AuthController@me');
-Route::resource('author', AuthorController::class);
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::get('ebook', 'App\Http\Controllers\BookController@index');
+Route::get('author', 'App\Http\Controllers\AuthorController@index');
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('ebook', 'App\Http\Controllers\BookController@store');
+    Route::put('ebook/{id}', 'App\Http\Controllers\BookController@update');
+    Route::delete('ebook/{id}', 'App\Http\Controllers\BookController@destroy');
+
+    Route::post('author', 'App\Http\Controllers\AuthorController@store');
+    Route::put('author/{id}', 'App\Http\Controllers\AuthorController@update');
+    Route::delete('author/{id}', 'App\Http\Controllers\AuthorController@destroy');
+
+
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+});
+// Route::resource('author', AuthorController::class);
 
  
